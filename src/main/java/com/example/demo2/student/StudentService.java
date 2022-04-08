cf10a59c8e541cfce3703634389b4415158a26c8
laurencetuchin <laurencetuchin@gmail.com>
2022-04-08 17:26:42 +1000

    Add semantic dependency injection

--- src/main/java/com/example/demo2/student/StudentService.java
+++ src/main/java/com/example/demo2/student/StudentService.java
@@ -1,8 +1,12 @@
 package com.example.demo2.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Service
 public class StudentService {
     public List<Student> getStudents() {
         return List.of(new Student(1L, "Tom", "tomhiddelston@test.com", LocalDate.of(1981,1,1),42));
