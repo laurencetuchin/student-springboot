6f1f11d51b8f475ac2ade46cdc8e5851b4c9b578
laurencetuchin <laurencetuchin@gmail.com>
2022-04-11 19:34:08 +1000

    Add transient age info

--- src/main/java/com/example/demo2/student/StudentConfig.java
+++ src/main/java/com/example/demo2/student/StudentConfig.java
@@ -15,8 +15,8 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
-            Student tom = new Student("Tom", "tomh@test.com", LocalDate.of(2000, 1, 1), 21);
-            Student loki = new Student("Loki", "loki@test.com", LocalDate.of(1900, 1, 1), 21);
+            Student tom = new Student("Tom", "tomh@test.com", LocalDate.of(2000, 1, 1));
+            Student loki = new Student("Loki", "loki@test.com", LocalDate.of(1900, 1, 1));
 
         };
 
