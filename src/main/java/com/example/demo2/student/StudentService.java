e713bc7fb5565ad8f0749a18f787ceb14e23ff1c
laurencetuchin <laurencetuchin@gmail.com>
2022-04-11 19:26:40 +1000

    Configure JPA

--- src/main/java/com/example/demo2/student/StudentService.java
+++ src/main/java/com/example/demo2/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo2.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +9,16 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
     public List<Student> getStudents() {
-        return List.of(new Student(1L, "Tom", "tomhiddelston@test.com", LocalDate.of(1981,1,1),42));
+        return studentRepository.findAll();
     }
 }
